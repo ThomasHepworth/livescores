e60f34ab9c72cfb3de783aefbe6e965e61be0757
Tom Hepworth <44782232+th368@users.noreply.github.com>
2021-09-26 22:42:14 +0100

    remove write_to_s3

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ from botocore.client import Config
 import pandas
 import json
 
-ACCESS_KEY_ID = 'AKIAJV4RWLDFYXCBP7XQ'
-ACCESS_SECRET_KEY = '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'
-BUCKET_NAME = 'livescores-jeffbot'
 
 def js_r(filename: str):
     with open(filename) as f_in:
@@ -27,19 +24,6 @@ def create_old_scores():
     # write to a new json file, "old_scores"
     writeToJSONFile('', 'old_scores', old_scores)
     print("old_scores successfully written to json")
-
-
-def write_to_s3():
-    s3 = boto3.resource(
-        's3',
-        aws_access_key_id=ACCESS_KEY_ID,
-        aws_secret_access_key=ACCESS_SECRET_KEY,
-        config=Config(signature_version='s3v4')
-    )
-    # write to s3
-    s3.Bucket(BUCKET_NAME).upload_file('old_scores.json', 'current_scores.json')
-
-    print("Sent files to S3 successfully")
 
 
 # setup process to output to a json file
