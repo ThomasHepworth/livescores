5f2bc4c29df3114166074aec81495687547ca6c5
th368 <tom_hepworth@hotmail.co.uk>
2021-09-26 23:10:07 +0100

    add change checker to scraper

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from scrapy.crawler import CrawlerProcess
 from apscheduler.schedulers.twisted import TwistedScheduler
 from livescoresSpider import flashscoreSpider # import our spider
-import boto3
-from botocore.client import Config
-import pandas
+from finalscoresSpider import fulltimeSpider # import our fulltime spider
 import json
-
-ACCESS_KEY_ID = 'AKIAJV4RWLDFYXCBP7XQ'
-ACCESS_SECRET_KEY = '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'
-BUCKET_NAME = 'livescores-jeffbot'
+from current_scores import print_pretty_scores_data
+from check_livescores_difference import find_latest_scores_data
+import time
 
 def js_r(filename: str):
     with open(filename) as f_in:
@@ -28,19 +25,40 @@ def create_old_scores():
     writeToJSONFile('', 'old_scores', old_scores)
     print("old_scores successfully written to json")
 
+def create_new_scores_v2():
+    # read in our previous scores
+    if __name__ == "__main__":
+        current_scores = js_r('current_scores.json')
+    # write to a new json file, "old_scores"
+    writeToJSONFile('', 'current_scores_v2', current_scores)
+    print("new_scores successfully written to json")
 
-def write_to_s3():
-    s3 = boto3.resource(
-        's3',
-        aws_access_key_id=ACCESS_KEY_ID,
-        aws_secret_access_key=ACCESS_SECRET_KEY,
-        config=Config(signature_version='s3v4')
-    )
-    # write to s3
-    s3.Bucket(BUCKET_NAME).upload_file('old_scores.json', 'current_scores.json')
+create_new_scores_v2() # run this to initialise our current_scores_v2.json file
 
-    print("Sent files to S3 successfully")
+def run_scrape():
+    # read in our previous scores
+    if __name__ == "__main__":
+        old_scores = js_r('current_scores_v2.json')
+    # write to a new json file, "old_scores"
+    writeToJSONFile('', 'old_scores', old_scores)
+    print("Old scores successfully written to json file - old_scores.json")
 
+
+    process.crawl(flashscoreSpider)
+    time.sleep(1) # give it a second to sleep...
+
+    # read in our previous scores
+    if __name__ == "__main__":
+        current_scores = js_r('current_scores.json')
+    # write to a new json file, "old_scores"
+    writeToJSONFile('', 'current_scores_v2', current_scores)
+    print("New scores successfully written to json file - current_scores_v2.json")
+
+    score_changes = find_latest_scores_data()
+    print_pretty_scores_data(score_changes['new_games'])
+    print_pretty_scores_data(score_changes['new_league'])
+    print_pretty_scores_data(score_changes['score_change'])
+    print("Checked")
 
 # setup process to output to a json file
 process = CrawlerProcess(settings={
@@ -50,10 +68,15 @@ process = CrawlerProcess(settings={
     },
 })
 
+process_fulltime = CrawlerProcess(settings={
+    "FEEDS": {
+        "final_scores.json": {"format": "json",
+                                "overwrite": "true"},
+    },
+})
 # set up our scheduler and time between each run
 scheduler = TwistedScheduler()
-scheduler.add_job(create_old_scores, 'interval', id = 'create_old_scores', seconds=10)
-scheduler.add_job(process.crawl, 'interval', args=[flashscoreSpider], id = 'scrape_scores_data', seconds=10)
-# scheduler.add_job(write_to_s3, 'interval', id = 'write_to_s3', seconds=10)
+scheduler.add_job(run_scrape, 'interval', id = 'checker', seconds=15)
+# scheduler.add_job(process_fulltime.crawl, 'interval', args=[fulltimeSpider], id = 'scrape_fulltime_scores_data', minutes=30, max_instances=1)
 scheduler.start()
 process.start(False)
